4ff39279896ed7ffcbe00cc6e971ae3aabf21849
jacobsthompson <jacob.s.thompson@icloud.com>
2026-03-13 16:36:55 -0700

    V0.4: Added River Cleanup, PASC Video Linked, Modal Changes, and Star Seeded Randomization
    Implemented:
    - Animated Bitmap Galaxy in center of screen
    - Starry Background with randomized pixel fading in and out
    - Header with name
    - Start of side panel / about me
    - Orbiting Planet / Project Orbs
    - Added Iconography for Planets
    - Added Planet Hover Titles
    - Added Planet Hover Highlight/Size Increase
    - Added On-Load Planet Animation
    - *Improved Rendering of Galaxy to only require 2 pngs instead of 90
    - Info / Description Cards added
    - Info Card on visbile animation added
    - 4/6 Description Text Finished
    - Dominos, Burgers, and SMW all link to their websites
    - Modals Added
    - Digital Duck Embedded Modal Added
    - LinkedIn Button
    - Github Button
    - Email Button
    - River Cleanup Project
    - Youtube Video Modal
    - Seeded Star Randomization on Refresh (Fixed bug where modal toggle would change stars)
    To Implement:
    - Graphic Design Modals
    - Contact Info
    - Astronaut
    - Shooting Stars

--- jacobthompson-website/src/javascripts/InfoCard.js
+++ jacobthompson-website/src/javascripts/InfoCard.js
@@ -1,15 +1,17 @@
 import '../stylesheets/infocard.css';
 
 export default function InfoCard({title, year, description}){
-    return (
-        <div className="info-wrapper">
-            <div className="info-card">
-                <div className="info-header">
-                    <div className="info-title">{title}</div>
-                    <div className="info-year">{year}</div>
+    if(title){
+        return (
+            <div className="info-wrapper">
+                <div className="info-card">
+                    <div className="info-header">
+                        <div className="info-title">{title}</div>
+                        <div className="info-year">{year}</div>
+                    </div>
+                    <div className="info-desc">{description}</div>
                 </div>
-                <div className="info-desc">{description}</div>
             </div>
-        </div>
-    );
+        );
+    }
 }
--- jacobthompson-website/src/javascripts/MainPage.js
+++ jacobthompson-website/src/javascripts/MainPage.js
@@ -8,15 +8,15 @@ import '../stylesheets/mainpage.css';
 
 const srcs = [
     ['Daily Dominos', 'assets/planets/planet-Domino.png', 'A Web Daily Game Developed in React Javascript.', '2026', 'https://dailydominos.vercel.app/', 'external'],
-    ['PASC', 'assets/planets/planet-PASC.png', 'Desc', '2024', 'https://www.youtube.com/embed/2DXrCV1i6EI', 'internal'],
-    ['Sunset Media Wave', 'assets/planets/planet-SMW.png', 'Desc', '2021-', 'https://www.sunsetmediawave.org/', 'external'],
     ['Balloons and Burgers', 'assets/planets/planet-BK.png', 'A web project that links the live location of WindBorne Solution weather balloons with the geolocations of Burger Kings around the world. Programmed with Node.js, MapLibre GL, JSON, Python, and the WindBorne API.', '2025', 'https://balloons-and-burgers.vercel.app/', 'external'],
-    ['', 'assets/planets/planetX.png', '', '', null, null],
+    ['Sunset Media Wave', 'assets/planets/planet-SMW.png', 'Desc', '2021-', 'https://www.sunsetmediawave.org/', 'external'],
+    ['PASC', 'assets/planets/planet-PASC.png', 'Desc', '2024', 'https://www.youtube.com/embed/2DXrCV1i6EI', 'internal'],
+    [null, 'assets/planets/planetX.png', null, null, null, null],
     ['UC Merced', 'assets/planets/planet-UCM.png', 'My portfolio of promotional assets I made as the Team Lead and Graphic Designer for the UC Merced Marketing Communications Social Media Team. Designs include fliers, logos, thumbnails, and social media posts. Made in Adobe Illustrator.', '2023-2025', null, 'internal'],
-    ['Graphic Design', 'assets/planets/planet-Ai.png', 'My portfolio of freelance Graphic Design made over the years from a wide variety of clients. Made in Adobe Illustrator and Adobe Photoshop.', '2019-', null, 'internal'],
     ['A Digital Duck', 'assets/planets/planet-Duck.png', 'A project developed during my freshman year Creative Coding class. An interactive simulator of a AI duck that eats, swims, walks, and quacks. Pick up the ducks or feed them breadcrumbs. Programmed with Processing Java and P5.JS.', '2021', "https://editor.p5js.org/jacobsthompson/full/KRplqbcFJ", 'internal'],
     ['River Cleanup', 'assets/planets/planet-River.png', 'Desc', '2022', "https://editor.p5js.org/jacobsthompson/full/eJ2FqNfXT", 'internal'],
-    ['', 'assets/planets/planetX-1.png', '', '', null, null]
+    ['Graphic Design', 'assets/planets/planet-Ai.png', 'My portfolio of freelance Graphic Design made over the years from a wide variety of clients. Made in Adobe Illustrator and Adobe Photoshop.', '2019-', null, 'internal'],
+    [null, 'assets/planets/planetX-1.png', null, null, null, null]
 ];
 
 const starRandomizedValue = new Date().toString();
