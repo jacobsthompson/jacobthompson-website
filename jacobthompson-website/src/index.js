1cefb79855172f3465fd1e08f79988cf037c1e58
jacobsthompson <jacob.s.thompson@icloud.com>
2026-03-18 11:24:19 -0700

    V0.5.1: Added Vercel Analytics
    Implemented:
    - Animated Bitmap Galaxy in center of screen
    - Starry Background with randomized pixel fading in and out
    - Header with name
    - Start of side panel / about me
    - Orbiting Planet / Project Orbs
    - Added Iconography for Planets
    - Added Planet Hover Titles
    - Added Planet Hover Highlight/Size Increase
    - Added On-Load Planet Animation
    - *Improved Rendering of Galaxy to only require 2 pngs instead of 90
    - Info / Description Cards added
    - Info Card on visbile animation added
    - 4/6 Description Text Finished
    - Dominos, Burgers, and SMW all link to their websites
    - Modals Added
    - Digital Duck Embedded Modal Added
    - LinkedIn Button
    - Github Button
    - Email Button
    - River Cleanup Project
    - Youtube Video Modal
    - Seeded Star Randomization on Refresh (Fixed bug where modal toggle would change stars)
    - Graphic Design Modals
    - Image Carousels
    - Full Screenable Images
    - Screen size scaling
    - Titles and Exit Button on Modals
    - Analytics
    To Implement:
    - Contact Info
    - Freelance work
    - SMW Description
    - Description in Modals
    - Astronaut (About Me)
    - Modal Animation (Info Card -> Modal)
    - Mobile Interface (Info Cards)
    - Shooting Stars

--- jacobthompson-website/src/index.js
+++ jacobthompson-website/src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import { Analytics } from "@vercel/analytics/next"
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Analytics/>
   </React.StrictMode>
 );
